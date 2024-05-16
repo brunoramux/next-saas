4b0fae5a20069b0058f7160d5e19e7db17f74d27
brunoramux <bruno.lemos@live.com>
2024-05-16 19:55:06 -0300

    CASL configuration as a package

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,42 @@
+import {
+  AbilityBuilder,
+  CreateAbility,
+  createMongoAbility,
+  ForcedSubject,
+  MongoAbility,
+} from '@casl/ability'
+
+import { User } from './models/user'
+import { permissions } from './permissions'
+
+const actions = ['manage', 'invite', 'delete'] as const
+const subjects = ['User', 'all'] as const
+
+type AppAbilities = [
+  (typeof actions)[number],
+  (
+    | (typeof subjects)[number]
+    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
+  ),
+]
+
+export type AppAbility = MongoAbility<AppAbilities>
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
+
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
+
+  if (typeof permissions[user.role] !== 'function') {
+    throw new Error(`Permissions for role ${user.role} not found.`)
+  }
+
+  permissions[user.role](user, builder)
+
+  const ability = builder.build()
+  return ability
+
+  // can('invite', 'User')
+  // cannot('delete', 'User')
+
+  // export const ability = build()
+}
