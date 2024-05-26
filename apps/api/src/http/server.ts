e7a1274a6a17615aad934075a810bf2867da7af9
Bruno Ramos Lemos <bruno.lemos@live.com>
2024-05-25 23:35:08 -0300

    Get Profile route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -12,6 +12,7 @@ import {
 
 import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { createAccount } from './routes/auth/create-account'
+import { getProfile } from './routes/auth/get-profile'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -41,6 +42,7 @@ app.register(fastifyJwt, {
 app.register(fastifyCors)
 app.register(createAccount)
 app.register(authenticateWithPassword)
+app.register(getProfile)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP SERVER RUNNING')
