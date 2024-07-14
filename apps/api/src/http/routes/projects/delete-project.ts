8858ffd8a98fe0c30bb28b93b52c3672722edf15
Bruno Ramos Lemos <bruno.lemos@live.com>
2024-07-14 19:09:50 -0300

    Add: route to delete projects

---
+++ apps/api/src/http/routes/projects/delete-project.ts
@@ -0,0 +1,78 @@
+import { defineAbilityFor } from '@saas/auth'
+import { projectSchema } from '@saas/auth/src/models/project'
+import { userSchema } from '@saas/auth/src/models/user'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function deleteProject(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .delete(
+      '/organizations/:slug/projects/:projectId',
+      {
+        schema: {
+          tags: ['projects'],
+          summary: 'Create a new Project',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+            projectId: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+        const { slug, projectId } = request.params
+        const { membership, organization } =
+          await request.getUserMembership(slug)
+
+        const project = await prisma.project.findUnique({
+          where: {
+            id: projectId,
+            organizationId: organization.id,
+          },
+        })
+
+        if (!project) {
+          throw new BadRequestError('Project not found.')
+        }
+
+        const authUser = userSchema.parse({
+          id: userId,
+          role: membership.role,
+        })
+
+        const authProject = projectSchema.parse({
+          id: projectId,
+          ownerId: userId,
+        })
+
+        const { cannot } = defineAbilityFor(authUser)
+
+        if (cannot('delete', authProject)) {
+          throw new UnauthorizedError(
+            'You are not allowed to delete this project.',
+          )
+        }
+
+        await prisma.project.delete({
+          where: {
+            id: projectId,
+          },
+        })
+
+        return reply.status(204).send()
+      },
+    )
+}
