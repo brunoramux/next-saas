0223ea79f0f6350abf7508e239ed6ebb23213fea
Bruno Ramos Lemos <bruno.lemos@live.com>
2024-07-14 20:40:10 -0300

    Add: route to get projects from a organization

---
+++ apps/api/src/http/routes/projects/get-projects.ts
@@ -0,0 +1,97 @@
+import { defineAbilityFor } from '@saas/auth'
+import { userSchema } from '@saas/auth/src/models/user'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function getProjects(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/projects',
+      {
+        schema: {
+          tags: ['projects'],
+          summary: 'Get all organization project',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              projects: z.array(
+                z.object({
+                  id: z.string().uuid(),
+                  name: z.string(),
+                  description: z.string(),
+                  slug: z.string(),
+                  ownerId: z.string().uuid(),
+                  avatarUrl: z.string().nullable(),
+                  organizationId: z.string().uuid(),
+                  createdAt: z.date(),
+                  owner: z.object({
+                    id: z.string().uuid(),
+                    name: z.string().nullable(),
+                    avatarUrl: z.string().nullable(),
+                  }),
+                }),
+              ),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+        const { slug } = request.params
+        const { membership, organization } =
+          await request.getUserMembership(slug)
+
+        const authUser = userSchema.parse({
+          id: userId,
+          role: membership.role,
+        })
+
+        const { cannot } = defineAbilityFor(authUser)
+
+        if (cannot('get', 'Project')) {
+          throw new UnauthorizedError(
+            'You are not allowed to see this project.',
+          )
+        }
+
+        const projects = await prisma.project.findMany({
+          select: {
+            id: true,
+            name: true,
+            description: true,
+            slug: true,
+            ownerId: true,
+            avatarUrl: true,
+            organizationId: true,
+            createdAt: true,
+            owner: {
+              select: {
+                id: true,
+                name: true,
+                avatarUrl: true,
+              },
+            },
+          },
+          where: {
+            organizationId: organization.id,
+          },
+          orderBy: {
+            createdAt: 'desc',
+          },
+        })
+
+        return reply.status(200).send({ projects })
+      },
+    )
+}
