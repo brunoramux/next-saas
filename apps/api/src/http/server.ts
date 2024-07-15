3795dd46f84a074ea9ade2d273c775e53e862c6c
Bruno Ramos Lemos <bruno.lemos@live.com>
2024-07-15 20:38:08 -0300

    Add: route for Accept an invite

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -20,6 +20,7 @@ import { requestPasswordRecovery } from './routes/auth/request-password-recovery
 import { resetPassword } from './routes/auth/reset-password'
 import { createInvite } from './routes/invites/create-invite'
 import { getInvite } from './routes/invites/get-invite'
+import { getInvites } from './routes/invites/get-invites'
 import { getMembers } from './routes/members/get-members'
 import { removeMember } from './routes/members/remove-member'
 import { updateMember } from './routes/members/update-member'
@@ -95,6 +96,7 @@ app.register(updateMember)
 app.register(removeMember)
 app.register(createInvite)
 app.register(getInvite)
+app.register(getInvites)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(
