fa767d07e1c6cecff9fb2f2922e06da864e99cd9
Bruno Ramos Lemos <bruno.lemos@live.com>
2024-07-15 18:14:02 -0300

    Add: route for Get information about an invite

---
+++ apps/api/src/http/routes/invites/get-invite.ts
@@ -0,0 +1,82 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function getInvite(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/invites/:inviteId',
+      {
+        schema: {
+          tags: ['invites'],
+          summary: 'Create a new Invite',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            inviteId: z.string().uuid(),
+          }),
+          response: {
+            200: z.object({
+              invite: z.object({
+                organization: z.object({
+                  name: z.string(),
+                }),
+                id: z.string().uuid(),
+                email: z.string().email(),
+                role: z.string(),
+                createdAt: z.date(),
+                author: z
+                  .object({
+                    id: z.string().uuid(),
+                    name: z.string().nullable(),
+                    avatarUrl: z.string().nullable(),
+                  })
+                  .nullable(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { inviteId } = request.params
+
+        const invite = await prisma.invite.findUnique({
+          where: {
+            id: inviteId,
+          },
+          select: {
+            id: true,
+            email: true,
+            role: true,
+            createdAt: true,
+            author: {
+              select: {
+                id: true,
+                name: true,
+                avatarUrl: true,
+              },
+            },
+            organization: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        })
+
+        if (!invite) {
+          throw new BadRequestError('Invite not found')
+        }
+
+        return reply.status(200).send({
+          invite,
+        })
+      },
+    )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -19,6 +19,7 @@ import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecovery } from './routes/auth/request-password-recovery'
 import { resetPassword } from './routes/auth/reset-password'
 import { createInvite } from './routes/invites/create-invite'
+import { getInvite } from './routes/invites/get-invite'
 import { getMembers } from './routes/members/get-members'
 import { removeMember } from './routes/members/remove-member'
 import { updateMember } from './routes/members/update-member'
@@ -93,6 +94,7 @@ app.register(getMembers)
 app.register(updateMember)
 app.register(removeMember)
 app.register(createInvite)
+app.register(getInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(
