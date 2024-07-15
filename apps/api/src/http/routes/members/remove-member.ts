a7890b245c8b501047b666b10490d936b7fac15b
Bruno Ramos Lemos <bruno.lemos@live.com>
2024-07-14 21:15:56 -0300

    Add: route to delete a member

---
+++ apps/api/src/http/routes/members/remove-member.ts
@@ -0,0 +1,61 @@
+import { defineAbilityFor } from '@saas/auth'
+import { userSchema } from '@saas/auth/src/models/user'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function deleteMember(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .delete(
+      '/organizations/:slug/members/:memberId',
+      {
+        schema: {
+          tags: ['projects'],
+          summary: 'Remove a Member from Organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+            memberId: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+        const { slug, memberId } = request.params
+        const { membership, organization } =
+          await request.getUserMembership(slug)
+
+        const authUser = userSchema.parse({
+          id: userId,
+          role: membership.role,
+        })
+
+        const { cannot } = defineAbilityFor(authUser)
+
+        if (cannot('delete', 'User')) {
+          throw new UnauthorizedError(
+            'You are not allowed to delete a member from this organization.',
+          )
+        }
+
+        await prisma.member.delete({
+          where: {
+            id: memberId,
+            organizationId: organization.id,
+          },
+        })
+
+        return reply.status(204).send()
+      },
+    )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -19,6 +19,7 @@ import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecovery } from './routes/auth/request-password-recovery'
 import { resetPassword } from './routes/auth/reset-password'
 import { getMembers } from './routes/members/get-members'
+import { deleteMember } from './routes/members/remove-member'
 import { updateMember } from './routes/members/update-member'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
@@ -89,6 +90,7 @@ app.register(getProjects)
 app.register(updateProject)
 app.register(getMembers)
 app.register(updateMember)
+app.register(deleteMember)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(
