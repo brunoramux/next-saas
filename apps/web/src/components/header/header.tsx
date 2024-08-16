e0bd63e329226f940ec8d344fc38ee2ebf6db735
Bruno Ramos Lemos <bruno.lemos@live.com>
2024-08-16 17:44:30 -0300

    Add organization switcher

--- apps/web/src/components/header/header.tsx
+++ apps/web/src/components/header/header.tsx
@@ -1,7 +1,9 @@
+import { Slash } from 'lucide-react'
 import Image from 'next/image'
 
 import logo from '@/assets/logo.svg'
 
+import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
 export function Header() {
@@ -9,7 +11,10 @@ export function Header() {
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
         <Image src={logo} className="size-12" alt="logo" />
+        <Slash className="size-6 -rotate-[24deg] text-border" />
+        <OrganizationSwitcher />
       </div>
+
       <div className="flex items-center gap-4">
         <ProfileButton />
       </div>
--- apps/web/src/components/header/organization-switcher.tsx
+++ apps/web/src/components/header/organization-switcher.tsx
@@ -1,1 +1,50 @@
-export function OrganizationSwitcher() {}
+import { DropdownMenuLabel } from '@radix-ui/react-dropdown-menu'
+import { ChevronsUpDown, PlusCircle } from 'lucide-react'
+import Link from 'next/link'
+
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu'
+
+export function OrganizationSwitcher() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex w-[168px] items-center gap-2 rounded p-1 text-sm font-medium outline-none focus-visible:ring-2 focus-visible:ring-primary">
+        <span className="text-muted-foreground">Select organization</span>
+        <ChevronsUpDown className="dize-4 ml-auto text-muted-foreground" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-[200px]"
+        align="end"
+        alignOffset={-16}
+        sideOffset={12}
+      >
+        <DropdownMenuGroup>
+          <DropdownMenuLabel className="mb-1 ml-1 text-sm">
+            Organizations
+          </DropdownMenuLabel>
+          <DropdownMenuItem>
+            <Avatar className="mr-2 size-6">
+              <AvatarImage src="https://github.com/rocketseat.png" />
+              <AvatarFallback />
+            </Avatar>
+            <span className="line-clamp-1">Rocketseat</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/create-organization">
+            <PlusCircle className="mr-2 size-6" />
+            Create new
+          </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
