47ec75be7d757ecdb5c6d19cbe027be0b72a20e0
Bruno Ramos Lemos <bruno.lemos@live.com>
2024-09-03 18:17:02 -0300

    Add: switcher theme from next-themes

--- apps/web/src/components/header/header.tsx
+++ apps/web/src/components/header/header.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image'
 import logo from '@/assets/logo.svg'
 import { ability } from '@/auth/auth'
 
+import { ThemeSwitcher } from '../theme/theme-switcher'
+import { Separator } from '../ui/separator'
 import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
@@ -13,12 +15,14 @@ export async function Header() {
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
         <Image src={logo} className="size-12" alt="logo" />
-        <Slash className="size-6 -rotate-[24deg] text-border" />
+        <Separator orientation="vertical" className="h-5" />
         <OrganizationSwitcher />
         {permissions?.can('get', 'Project') && <p>Projetos</p>}
       </div>
 
       <div className="flex items-center gap-4">
+        <ThemeSwitcher />
+        <Separator orientation="vertical" className="h-5" />
         <ProfileButton />
       </div>
     </div>
