b76b5d56119057c1084aefa355e5f78060174149
Bruno Ramos Lemos <bruno.lemos@live.com>
2024-09-06 17:00:26 -0300

    Checking permissions to create projects

--- apps/web/src/app/(apps)/org/[slug]/create-project/page.tsx
+++ apps/web/src/app/(apps)/org/[slug]/create-project/page.tsx
@@ -1,8 +1,17 @@
+import { redirect } from 'next/navigation'
+
+import { ability } from '@/auth/auth'
 import { Header } from '@/components/header/header'
 
 import CreateProjectForm from './create-project-form'
 
-export default function CreateProject() {
+export default async function CreateProject() {
+  const permissions = await ability()
+
+  if (permissions?.cannot('create', 'Project')) {
+    redirect('/')
+  }
+
   return (
     <div className="space-y-4">
       <Header />
