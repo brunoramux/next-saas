702d9ac718c44b3f8675fbaa49763252375a468b
Bruno Ramos Lemos <bruno.lemos@live.com>
2024-09-22 10:22:08 -0300

    Revoke members invite

--- apps/web/src/app/(apps)/org/[slug]/members/actions.ts
+++ apps/web/src/app/(apps)/org/[slug]/members/actions.ts
@@ -1,10 +1,14 @@
 'use server'
 
-import type { Role } from '@saas/auth/src/roles'
+import { type Role, roleSchema } from '@saas/auth/src/roles'
+import { HTTPError } from 'ky'
 import { revalidateTag } from 'next/cache'
+import { z } from 'zod'
 
 import { getCurrentOrg } from '@/auth/auth'
+import { createInvite } from '@/http/create-invite'
 import { removeMember } from '@/http/remove-member'
+import { revokeInvite } from '@/http/revoke-invite'
 import { updateMember } from '@/http/update-member'
 
 export async function removeMemberAction(memberId: string) {
@@ -21,4 +25,63 @@ export async function updateMemberAction(memberId: string, role: Role) {
   await updateMember({ memberId, role, slug: currentOrg! })
 
   revalidateTag(`${currentOrg}/members`)
+}
+
+const inviteSchema = z.object({
+  email: z.string().email({ message: 'Invalid e-mail address.' }),
+  role: roleSchema,
+})
+
+export async function createInviteAction(data: FormData) {
+  const currentOrg = getCurrentOrg()!
+  const result = inviteSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+
+  const { email, role } = result.data
+
+  try {
+    await createInvite({
+      org: currentOrg,
+      email,
+      role,
+    })
+
+    revalidateTag(`${currentOrg}/invites`)
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.error(err)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+
+  return {
+    success: true,
+    message: 'Successfully created the invite.',
+    errors: null,
+  }
+}
+
+export async function revokeInviteAction(inviteId: string) {
+  const currentOrg = getCurrentOrg()
+
+  await revokeInvite({
+    org: currentOrg!,
+    inviteId,
+  })
+
+  revalidateTag(`${currentOrg}/invites`)
 }
---
+++ apps/web/src/http/get-invites.ts
@@ -0,0 +1,28 @@
+import type { RoleExt } from '@saas/auth'
+
+import { api } from './api-client'
+
+interface GetInvitesResponse {
+  invites: {
+    id: string
+    role: RoleExt
+    email: string
+    createdAt: string
+    author: {
+      id: string
+      name: string | null
+    } | null
+  }[]
+}
+
+export async function getInvites(org: string) {
+  const result = await api
+    .get(`organizations/${org}/invites`, {
+      next: {
+        tags: [`${org}/invites`],
+      },
+    })
+    .json<GetInvitesResponse>()
+
+  return result
+}
