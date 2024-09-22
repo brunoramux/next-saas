7f3cdeebfd2e203a4790c1a4c59a58b6cc63a109
Bruno Ramos Lemos <bruno.lemos@live.com>
2024-09-22 15:35:09 -0300

    Add: projects list

---
+++ apps/web/src/app/(apps)/org/[slug]/(projects)/project-list.tsx
@@ -0,0 +1,52 @@
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import { ArrowRight } from 'lucide-react'
+
+import { getCurrentOrg } from '@/auth/auth'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { getProjects } from '@/http/get-projects'
+
+dayjs.extend(relativeTime)
+
+export async function ProjectList() {
+  const org = getCurrentOrg()
+  const { projects } = await getProjects(org!)
+
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {projects.map((project) => (
+        <Card key={project.id} className="flex flex-col justify-between">
+          <CardHeader>
+            <CardTitle>{project.name}</CardTitle>
+            <CardDescription className="line-clamp-2 leading-relaxed">
+              {project.description}
+            </CardDescription>
+          </CardHeader>
+          <CardFooter className="flex items-center gap-1.5">
+            <Avatar className="size-4">
+              <AvatarImage src={project.avatarUrl} />
+              <AvatarFallback />
+            </Avatar>
+            <span className="text-xs text-muted-foreground">
+              <span className="font-medium text-foreground">
+                {project.owner.name}
+              </span>{' '}
+              {dayjs(project.createdAt).fromNow()}
+            </span>
+            <Button size="xs" variant="outline" className="ml-auto">
+              View <ArrowRight className="ml-2 size-3" />
+            </Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  )
+}
